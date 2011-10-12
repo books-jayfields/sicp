f13f3cc1afef6dbcde4466048afccdbc7cd3aa6f
Jay Fields <git@jayfields.com>
2011-10-12 06:33:50 -0400

    first exercise

---
+++ test/sicp/test/exercises_1_2.clj
@@ -0,0 +1,11 @@
+(ns sicp.test.exercises-1-2
+  (use expectations))
+
+(expect (/ -37 150)
+        (/
+         (+ 5 4
+            (- 2
+               (- 3
+                  (+ 6
+                     (/ 4 5)))))
+         (* 3 (- 6 2) (- 2 7))))
